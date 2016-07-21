c3b06fb2df6d62cbbac8247a66574987054e6e1f
Jeff Buchbinder <jeff@ourexchange.net>
2016-07-21 13:42:56 -0400

    Adjustments to add imgcap tag and background images for index page.

--- plugins/image_tag.rb
+++ plugins/image_tag.rb
@@ -15,6 +15,18 @@
 # <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
 # <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
 #
+# Syntax {% imgcap [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
+#
+# Examples:
+# {% imgcap /images/ninja.png Ninja Attack! %}
+# {% imgcap left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% imgcap left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
+#
+# Output:
+# <figure><img src="/images/ninja.png"><figcaption></figcaption</figure>
+# <figure class="left half"><img src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!"><figcaption>Ninja Attack!</figcaption></figure>
+# <figure class="left half"><img src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture"><figcaption>Ninja Attack!</figcaption></figure>
+#
 
 module Jekyll
 
@@ -45,6 +57,26 @@ module Jekyll
       end
     end
   end
+
+  class CaptionImageTag < ImageTag
+    @class = ''
+    def initialize(tag_name, markup, tokens)
+      super
+    end
+
+    def render(context)
+      if @img
+        if @img['class']
+          @class = @img['class']
+          @img.delete('class')
+        end
+        "<figure class='#{@class}'><img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}><figcaption>#{@img['title']}</figcaption></figure>"
+      else
+        "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
+      end
+    end
+  end
 end
 
 Liquid::Template.register_tag('img', Jekyll::ImageTag)
+Liquid::Template.register_tag('imgcap', Jekyll::CaptionImageTag)
